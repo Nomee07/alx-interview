3abdd42462e4ae760abd4246ba16ebc515593d3e
Nomee07 <gigabanomfundo@gmail.com>
2024-06-05 13:16:12 +0000

    StarWars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status Code:', response.statusCode);
+  } else {
+    const film = JSON.parse(body);
+    const characters = film.characters;
+
+    characters.forEach(characterUrl => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error('Error:', error);
+        } else if (response.statusCode !== 200) {
+          console.error('Status Code:', response.statusCode);
+        } else {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    });
+  }
+});
