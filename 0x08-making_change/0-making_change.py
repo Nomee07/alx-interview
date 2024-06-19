1e5fbae1cbd1bbcdc5da42a9d8871a8fc7e30ff0
Nomee07 <gigabanomfundo@gmail.com>
2024-06-19 07:24:00 +0000

    Making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,17 +12,23 @@ def makeChange(coins, total):
     """
     if not coins:
         return -1
+
+    # If the total is 0 or negative, return 0 as no coins are needed
     if total <= 0:
         return 0
-    
+
     coins.sort(reverse=True)
     change = 0
-    
+
+    # Iterate over each coin in the sorted list
     for coin in coins:
         while coin <= total:
             total -= coin
             change += 1
+
+        # If the total is reduced to 0, return the count of coins used
         if total == 0:
             return change
-    
+
+    # If the total cannot be met by any number of coins, return -1
     return -1
